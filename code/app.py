b050701bc0405027f852a54df5f33e4e14946326
Malo <126970037+Malaudos35@users.noreply.github.com>
2025-05-18 22:16:12 +0200

    Create No-as_aservice api

---
+++ code/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, render_template
+import json
+import random
+
+app = Flask(__name__)
+
+def load_reasons(lang):
+    with open(f'reason/{lang}.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/random-reason/<lang>', methods=['GET'])
+def get_random_reason(lang):
+    try:
+        reasons = load_reasons(lang)
+        random_reason = random.choice(reasons)
+        response = jsonify({"reason": random_reason})
+        response.data = json.dumps({"reason": random_reason}, ensure_ascii=False)
+        response.headers['Content-Type'] = 'application/json; charset=utf-8'
+        return response
+    except FileNotFoundError:
+        return jsonify({"error": "Language not supported"}), 404
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ naas.Dockerfile
@@ -0,0 +1,22 @@
+# Utilise une image de base Python
+FROM python:3.9-slim
+
+# Définit le répertoire de travail dans le conteneur
+WORKDIR /app
+
+# Copie les fichiers de dépendances
+COPY requirements.txt .
+
+# Installe les dépendances
+RUN pip install --no-cache-dir -r requirements.txt
+RUN apt-get update
+RUN apt-get dist-upgrade -y
+
+# Copie le reste des fichiers de l'application
+COPY ./code/ .
+
+# Expose le port sur lequel l'application Flask écoute
+EXPOSE 5000
+
+# Commande pour exécuter l'application Flask
+CMD ["python", "app.py"]
