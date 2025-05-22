a05f600aedb47e5312bc8022b462238c1cd9628c
Malo <126970037+Malaudos35@users.noreply.github.com>
2025-05-22 21:48:37 +0200

    Add hire me as a service

--- code/app.py
+++ code/app.py
@@ -20,18 +20,49 @@ def load_reasons(lang):
     cached_reasons[lang] = data
     return data
 
+cached_hireme = {}
+
+def load_hireme(lang):
+    # Test if we have data in cache
+    if lang in cached_hireme:
+        return cached_hireme[lang]
+
+    # Get data from json file
+    with open(f'hireme/{lang}.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    # Put data in cache
+    cached_hireme[lang] = data
+    return data
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('reason.html')
 
 @app.route('/no')
-def home2():
-    return render_template('index.html')
+def no():
+    return render_template('reason.html')
 
 @app.route('/no/<lang>', methods=['GET'])
 def get_random_reason(lang):
     try:
         reasons = load_reasons(lang)
+        random_reason = random.choice(reasons)
+        response = jsonify({"reason": random_reason})
+        response.data = json.dumps({"reason": random_reason}, ensure_ascii=False)
+        response.headers['Content-Type'] = 'application/json; charset=utf-8'
+        return response
+    except FileNotFoundError:
+        return jsonify({"error": "Language not supported"}), 404
+    
+@app.route('/hireme')
+def hireme():
+    return render_template('hireme.html')
+
+@app.route('/hireme/<lang>', methods=['GET'])
+def get_random_hireme(lang):
+    try:
+        reasons = load_hireme(lang)
         random_reason = random.choice(reasons)
         response = jsonify({"reason": random_reason})
         response.data = json.dumps({"reason": random_reason}, ensure_ascii=False)
